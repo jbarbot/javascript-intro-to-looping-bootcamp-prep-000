3d43424dd9de018acd8a811f10037663408ede4d
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 15:24:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++)
-  array.push(`"I am ${i} strange loop${ i === 0 ? '' : 's'}."`)
+  array.push(`"I am ${i} strange loop${ i === 0 ? '' : 's'}."`);
 }
