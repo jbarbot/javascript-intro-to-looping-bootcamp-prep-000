4383ccc845a43152d7768464bf299c0e5dd6fcab
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 15:20:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++)
-  array.assign(`I am ${i+1} strange loop${ i === 0 ? '' : 's'}.`)
+  array.push(`I am ${i} strange loop${ i === 0 ? '' : 's'}.`)
 }
